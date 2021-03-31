b871bb10e1005506a71cd8c6df5e13fc19853376
Prince Nain <prince@loudcurtain.com>
2021-03-31 14:15:10 +0530

    again update tnblogo

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Fade from "react-reveal/Fade"
 import data from "../mydata"
+import prince from "../images/tnblogo.png"
 const About = () => {
   return (
     <div className="secion" id="about">
@@ -21,7 +22,7 @@ const About = () => {
             </p>
           </div>
           <div className="image-wrapper">
-            <img href="https://images.unsplash.com/photo-1472417583565-62e7bdeda490?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1268&q=80" alt="about"></img>
+            <img src={prince} alt="about"></img>
           </div>
         </div>
       </div>
