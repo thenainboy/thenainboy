53e6f7ade221e98e3bf051cbe8bfd63cc4a13f71
Prince Nain <prince@loudcurtain.com>
2021-03-31 11:33:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,76 @@
+## A clean, beautiful, minimal and responsive portfolio template for Developers!
+
+![Web 1920 – 1](https://user-images.githubusercontent.com/16558205/101065060-b44bfc80-35ba-11eb-8ab4-1e6f140c1ec9.png)
+
+
+
+
+Just change `src/mydata.js` to get your personal portfolio. Feel free to use it as-is or customize it as much as you want.
+
+
+
+
+
+## Features
+- Responsive
+- Modern Design
+- Clean Animations
+- Easy to use
+
+
+## Sections
+✔️ Header
+✔️ Work
+✔️ About
+✔️ Skills
+✔️ Contact
+
+
+## How To Use 
+
+From your command line, clone and run thenainboy:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/thenainboy/thenainboy
+
+# Go into the repository
+$ cd thenainboy
+
+# Install dependencies
+$ yarn
+
+#Start's development server
+$ gatsby develop
+```
+
+#### Personalize page content in `/src/yourdata.js` & modify it as per your need.
+
+```javascript
+/* Change this file to get your Personal Porfolio */
+ // Edit Values of these variables
+
+name:"Thenainboy",
+headerTagline: [
+    "Building digital",.... 
+]
+
+```
+
+## Deployment
+When you are done with the setup, you should host your website online.
+We highly recommend to read through the [Deploying on Github Pages](https://create-react-app.dev/docs/deployment/#github-pages) docs for React.
+
+
+## Technologies Used
+
+- [React](https://reactjs.org/)
+- [Gatsby](https://www.gatsbyjs.com/)
+
+
+
+
+
+## Thanks
+If you like this portfolio template don't forget give it a ⭐ and also share it with your friends
+
---
+++ src/mydata.js
@@ -0,0 +1,191 @@
+// Skills Icons
+import shoesIcon from "./images/shoes.svg"
+import clothIcon from "./images/cloth.svg"
+import foodIcon from "./images/food.svg"
+import socialmediaIcon from "./images/socialmedia.svg"
+import houseIcon from "./images/house.svg"
+import bookIcon from "./images/book.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "TheNainBoy Official",
+  headerTagline: [
+    //Line 1 For Header
+    "Building A Team",
+    //Line 2 For Header
+    "That Have The Same Dream",
+    //Line 3 For Header
+    "and experience",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "All our dreams can come true, if we have the courage to pursue them",
+
+  //Contact Email
+  contactEmail: "prince@loudcurtain.com" ,
+  
+  
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Project One", //Project Title - Add Project Title Here
+      para:
+        "Creating A Shoe Brand can be a crazy or may be looks imposible startup for 2021 but it has lots of potential as well this year because of lack of availbility of branded shoes in rural areas. #EveryOne Want Branded Things So Are You With me , Lets Be Provider .    ", // Add Service Type Here
+      //Project Image - Add Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1465453869711-7e174808ace9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80",
+      //Project URL - Add Project Url Here
+      url: "https://youtu.be/kYpIsJYWHQg",
+    },
+    {
+      title: "Project Two", //Project Title - Add Project Title Here
+      para:
+        "Garments industry", // Add Service Type Here
+      //Project Image - Add Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+      //Project URL - Add Project Url Here
+      url: "https://youtu.be/qjfUGaur188",
+    },
+    {
+      title: "Project Three", //Project Title - Add Project Title Here
+      para:
+        "Social Media Advertisement/Company", // Add Service Type Here
+      //Project Image - Add Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1562577309-4932fdd64cd1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Project Url Here
+      url: "https://youtu.be/ug_84s1tg48",
+    },
+    {
+      title: "Project Four", //Project Title - Add Project Title Here
+      para:
+        "HomeMade Food Business With Frenchise System", // Add Service Type Here
+      //Project Image - Add Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80://https://images.unsplash.com/photo-1536184071535-78906f7172c2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=820&q=80.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Project Url Here
+      url: "https://youtu.be/xxDvyT1gxBU",
+    },
+    {
+      title: "Project Five", //Project Title - Add Project Title Here
+      para:
+        "Real-Estate Business", // Add Service Type Here
+      //Project Image - Add Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/flagged/photo-1558954157-aa76c0d246c6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Project Url Here
+      url: "https://youtu.be/dnHllyeiFqE",
+    },
+    {
+      title: "Project Six", //Project Title - Add Project Title Here
+      para:
+        "E-School / Online Classes / Tutions / Ebooks etc. Business is On Next Level This Year ", // Add Service Type Here
+      //Project Image - Add Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Project Url Here
+      url: "https://youtu.be/k4FrwDi8ISw",
+    },
+
+    /*
+    If I Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "Let Me Tell you something about me before My Dreams, if You Want To work With me Then Please Let me be me..Because once I change, you will miss the ‘me’ in me .",
+  aboutParaTwo:
+    "We all have dreams. But in order to make dreams come into reality, it takes an awful lot of determination, dedication, self-discipline, and effort.",
+    aboutParaThree:
+    " I Want To Create Brand for Shoes , Clothing , Real-Estate & for Social Media Promotion Company as well as a HomeMade Food Company that will provide a tasty homemade food to the travelers arround the glode , not just locally.    ",
+  
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: shoesIcon,
+      para:
+        "1. Design 2. Production 3. Marketing ",
+    },
+    {
+      img: clothIcon,
+      para:
+        "1. High on Creativity and Artistic Skills 2. Excellent Drawing Ability 3. Strong Visualization Skills",
+    },
+    {
+      img: socialmediaIcon,
+      para:
+        "1. Excellent Communication 2. Creativity 3. Writing Ability",
+    },
+    {
+      img: foodIcon,
+      para:
+        " 1. Researching Competition 2. Marketing & Social Media Management 3. Recipe Knowledge 4. Business Skills",
+    },
+    {
+      img: houseIcon,
+      para:
+        "1.Active Listening 2.Negotiation 3. Patience",
+    },
+    {
+      img: bookIcon,
+      para:
+        "1.Identify your audience 2. Pick your cornerstone content 3. Plan your lessons",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Lets Begin",
+  promotionPara:
+    " I knew that if I failed I wouldn’t regret that, but I knew the one thing I might regret is not trying.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com" },
+    {
+      img: codepenIcon,
+      url: "https://www.codepen.com/",
+    },
+    
+    {
+      img: instagramIcon,
+      url: "https://www.instagram.com/",
+    },
+  ],
+
+  // End Contact Section ---------------
+}
+
