670520ad23b5f7730a6f029e0bece7536269bb55
Prince Nain <prince@loudcurtain.com>
2021-03-31 21:06:07 +0530

    add new tittle

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     pathPrefix: "/thenainboy",
 
-    title: `Gatsby Default Starter`,
+    title: `thenainboy`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
